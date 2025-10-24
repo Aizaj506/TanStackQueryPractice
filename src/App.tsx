025287bc63c221b80c25cbf0c90392678e2b6deb
Ahmed Ajaj <ahmed.ajaj@fyntune.com>
2025-10-24 11:47:25 +0530

    Tanstack Query Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import { useQuery } from "@tanstack/react-query"
 
 const App = () => {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['posts'],
+    queryFn: () =>
+      fetch('https://jsonplaceholder.typicode.com/posts').then(res => res.json()),
+  })
+
+  console.log({ data, error, isLoading })
+  if(isLoading) return <div> Loading .....</div>
+  if(error) return <div> An error occured: {(error as Error).message} </div>
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <h1 className="text-3xl font-bold underline">Posts</h1>
+      <ul>
+        {data?.map((post: {id: number; title: string}) => {
+          return <li key={post.id}>{post.title}</li>
+        })}
+      </ul>
+    </div>
   )
 }
 
